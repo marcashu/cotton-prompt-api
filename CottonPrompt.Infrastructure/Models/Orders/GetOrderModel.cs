1882c70988b0f12241eccf7553389c699e4db51a
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2024-03-23 21:54:01 +0800

    updated getorderbyid endpoint - added usergroup

--- CottonPrompt.Infrastructure/Models/Orders/GetOrderModel.cs
+++ CottonPrompt.Infrastructure/Models/Orders/GetOrderModel.cs
@@ -22,6 +22,7 @@ namespace CottonPrompt.Infrastructure.Models.Orders
         string CheckerStatus,
         string CustomerStatus,
         Guid? ArtistId,
-        Guid? CheckerId
+        Guid? CheckerId,
+        string UserGroup
     );
 }
